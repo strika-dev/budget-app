150ad24a4705e13b1fdf8222ed4db34268cd57a7
Aboubakrine Diallo <aboubakrine02@gmail.com>
2026-01-07 19:54:22 -0500

    buget-app final version

--- index.js
+++
@@ -1,6 +0,0 @@
-// Components barrel export
-export { Header } from './Header';
-export { Summary } from './Summary';
-export { TransactionForm } from './TransactionForm';
-export { TransactionList } from './TransactionList';
-export { Charts, CategoryChart, TrendChart } from './Charts';
